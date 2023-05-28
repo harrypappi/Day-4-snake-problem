29f413487f447dc50956b49f4c39eacaa9726dc5
“reachsumit1” <harry.pappi143@gmail.com>
2023-05-28 14:14:26 +0530

    UC2.cs

---
+++ UC2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Die
+{
+    private Random random;
+
+    public Die()
+    {
+        random = new Random();
+    }
+
+    public int Roll()
+    {
+        return random.Next(1, 7);
+    }
+}
+
+public class Player
+{
+    public void RollDie()
+    {
+        Die die = new Die();
+        int rollResult = die.Roll();
+
+        Console.WriteLine("The player rolled: " + rollResult);
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Player player = new Player();
+        player.RollDie();
+    }
+}
